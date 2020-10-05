6f38bd62ca5a6234f23c93c882cb590ec3aad0ac
Wojciech Cieslik <cieslikw.96@gmail.com>
2020-10-05 20:12:08 +0200

    game skeleton

--- battleship.py
+++ battleship.py
@@ -9,5 +9,42 @@ def print_board(board):
     for row in board:
         print((" ").join(row))
 
-board = starting_board(8,5) 
+def get_ship_place() #Pobieranie od uzytkownika miesca statków, zapisywanie i zwaracanie współrzędnych 
+    pass
+
+def get_player_move() #
+    pass
+
+def is_hit() 
+    pass
+
+def is_sunk()
+    pass
+
+def mark()
+    pass
+
+def has_won()
+    pass
+
+def print_result()
+    pass 
+
+def menu()
+    pass 
+
+def battleship_game()
+
+# board; players1_ships; player2_ships; player_1_moves; player_2_moves
+
+while has_won() == False
+
+
+
+
+
+board = starting_board() 
+
 print_board(board)
+
+
