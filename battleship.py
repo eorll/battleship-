9d112d0014ecab4aa4c9cf815c7e7aab97bba084
eorll <robert0dadon@gmail.com>
2020-10-08 18:45:23 +0200

    Added coordinates to board

--- battleship.py
+++ battleship.py
@@ -6,45 +6,64 @@ def starting_board(rows = 5, columns = 5):
     return board
 
 def print_board(board):
+    column_number = []
+    n = 0
+    for i in board[0]:
+        n += 1 
+        column_number.append(str(n))
+        column_number.append(' ')
+
+    print("  "+"".join(column_number))
+    row_number = 1
     for row in board:
-        print((" ").join(row))
+        print((str(row_number)) + " " + (" ").join(row))
+        row_number += 1
+    
 
-def get_ship_place() #Pobieranie od uzytkownika miesca statków, zapisywanie i zwaracanie współrzędnych 
+def get_ship_place(): #Pobieranie od uzytkownika miesca statków, zapisywanie i zwaracanie współrzędnych 
     pass
 
-def get_player_move() #
+def get_player_move(): 
     pass
 
-def is_hit() 
+def is_hit(): 
     pass
 
-def is_sunk()
+def is_sunk():
     pass
 
-def mark()
+def mark():
     pass
 
-def has_won()
+def has_won():
     pass
 
-def print_result()
+def print_result():
     pass 
 
-def menu()
+def menu():
     pass 
 
-def battleship_game()
+def battleship_game():
 
-# board; players1_ships; player2_ships; player_1_moves; player_2_moves
+# board; players_1_ships; player_2_ships; player_1_moves; player_2_moves
 
-while has_won() == False
+    player_1_moves = starting_board()
+    player_2_moves = starting_board()
+    players_1_ships = get_ship_place()
+    player_2_ships = get_ship_place()
+
+    while has_won() == False:
+        
+        break
+        
 
 
 
 
 
-board = starting_board() 
 
-print_board(board)
+
+
 
 
