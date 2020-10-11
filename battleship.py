bf70536207b86f38de9aeb6d3011b7158afe44d8
Kat.Davidson <70635747+KateDavidson@users.noreply.github.com>
2020-10-11 19:36:49 +0200

    Update battleship.py

--- battleship.py
+++ battleship.py
@@ -113,17 +113,7 @@ def play_again():
              input("Enter only numbers!")
 
 
- #def is_hit(player_X_ship, player_Y_moves, coordinates): 
-     # powinna brać tablice player_X_ships i koordynaty z get_player_move i zwracać hit lub miss na tablicy  player_Y_moves
-     #pass
 
- def is_sunk():
-     # na razie nie potrzebna
-     pass
-
- def mark():
-     # is_hit bierze to na siebie
-     pass
 
  def has_won():
      pass
@@ -175,42 +165,5 @@ if __name__ == "__main__":
 
 
 
-def get_ship_place() #Pobieranie od uzytkownika miesca statków, zapisywanie i zwaracanie współrzędnych 
-    pass
-
-def get_player_move() #
-    pass
-
-def is_hit() 
-    pass
-
-def is_sunk()
-    pass
-
-def mark()
-    pass
-
-def has_won()
-    pass
-
-def print_result()
-    pass 
-
-def menu()
-    pass 
-
-def battleship_game()
-
-# board; players1_ships; player2_ships; player_1_moves; player_2_moves
-
-while has_won() == False
-
-
-
-
-
-board = starting_board() 
-
-print_board(board)
 
 
